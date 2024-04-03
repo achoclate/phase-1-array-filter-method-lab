c2a4d0f39f0f6bfddbc04e0830d39afefa749ab8
achoclate <nyabokeann99@gmail.com>
2024-04-03 06:58:33 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+
+function findMatching(start, driver) {
+    return start.filter(
+      (possibleMatch) => possibleMatch.toLowerCase() === driver.toLowerCase()
+    );
+  }
+  
+  function fuzzyMatch(start, testString) {
+    return start.filter(
+      (possibleMatch) =>
+        possibleMatch.toLowerCase().indexOf(testString.toLowerCase()) === 0
+    );
+  }
+  
+  function matchName(start, driverName) {
+    return start.filter((record) => record.name === driverName);
+  }
